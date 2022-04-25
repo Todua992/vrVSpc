44358d34f7582c63f60143f4d0103564a6a28ec4
p.h.jessen45@gmail.com <>
2022-04-25 20:14:51 +0200

    Half way making cannon a network object

--- Assets/Scripts/SelfDestruct.cs
+++ Assets/Scripts/SelfDestruct.cs
@@ -1,24 +1,13 @@
 using UnityEngine;
 
 public class SelfDestruct : MonoBehaviour {
-    [SerializeField] private bool onCollisionDestroy = true;
-
     [SerializeField] private float timer;
 
-    private void OnCollisionEnter(Collision collision) {
-        if (onCollisionDestroy == true) {
-            Destroy(gameObject);
-
-        }
-    }
-
-    public void Update() {
+    private void Update() {
         if (timer > 0f) {
             timer -= Time.deltaTime;
-
-            if (timer <= 0f) {
-                Destroy(gameObject);
-            }
+        } else {
+            Destroy(gameObject);
         }
     }
 }
