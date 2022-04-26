1c32c8c9982f385ed0dd6e5c550d12f3e2b79304
p.h.jessen45@gmail.com <>
2022-04-26 11:02:22 +0200

    Made prototype cannon in multiplayer

--- Assets/Scripts/CannonShoot.cs
+++ Assets/Scripts/CannonShoot.cs
@@ -2,16 +2,35 @@ using Unity.Netcode;
 using UnityEngine;
 
 public class CannonShoot : MonoBehaviour {
+    [SerializeField] private float reloadTime;
     [SerializeField] private float shootSpeed;
     [SerializeField] private Transform cannonBallSpawn;
     [SerializeField] private GameObject cannonBallPrefab;
     [SerializeField] private AudioSource explosionSound;
     [SerializeField] private ParticleSystem explosionVFX;
 
+    [SerializeField] private NetworkVariable<bool> networkTimer = new NetworkVariable<bool>();
+    [SerializeField] private NetworkVariable<bool> networkShooting = new NetworkVariable<bool>();
+
+    private float timer = 0;
     private bool colliding = false;
+    private bool shooting = false;
 
     private void Update() {
-        if (colliding && Input.GetKeyDown(KeyCode.P)) {
+        if (networkTimer.Value) {
+            timer = reloadTime;
+            UpdateReloadTimeServerRcp(false);
+        }
+        
+        if (timer > 0) {
+            timer -= Time.deltaTime;
+        } else if (colliding && Input.GetKeyDown(KeyCode.P)) {
+            UpdateShootingServerRpc(true);
+            UpdateReloadTimeServerRcp(true);
+        }
+
+        if (shooting == true) {
+            UpdateShootingServerRpc(false);
             Shoot();
         }
     }
@@ -35,5 +54,15 @@ public class CannonShoot : MonoBehaviour {
         if (collider.CompareTag("Player")) {
             colliding = false;
         }
+    }
+
+    [ServerRpc]
+    public void UpdateReloadTimeServerRcp(bool reloadTimer) {
+        networkTimer.Value = reloadTimer;
+    }
+
+    [ServerRpc]
+    public void UpdateShootingServerRpc(bool shooting) {
+        networkShooting.Value = shooting;
     }
 }
