3c62abad3d07f5fb1898c018a339a8bb6ec8374a
lukagociridze16@gmail.com <>
2022-04-28 00:26:05 +0200

    Trees regrow when fallen from map

---
+++ Assets/TreeRegrow.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+
+public class TreeRegrow : MonoBehaviour
+{
+    [SerializeField] private float Scaleratio;
+
+    private Rigidbody rb;
+
+    private Vector3 currentScaleSpeed;
+    private Vector3 Startposition;
+    private Vector3 StartScale;
+
+    private Quaternion StartRotation;
+    
+    private bool newborn;
+    
+    void Start()
+    {
+        currentScaleSpeed = new Vector3 (currentScaleSpeed.x = Scaleratio, currentScaleSpeed.y = Scaleratio, currentScaleSpeed.z = Scaleratio);
+        newborn = false;
+    rb = GetComponent<Rigidbody>();
+        rb.isKinematic = true;
+        StartScale = transform.localScale;
+        Startposition = transform.position;
+        StartRotation = transform.rotation;
+
+    }
+
+    void Update()
+    {
+        if (transform.position.y <= -10)
+        {
+            Givebirth();
+        }
+
+        if(newborn == true)
+        {
+            transform.localScale = transform.localScale + currentScaleSpeed * Time.deltaTime;
+            if ((StartScale.x - transform.localScale.x) < 0)
+            {
+                newborn=false;
+            }
+        }
+        
+    }
+
+
+
+    void Givebirth()
+    {
+        transform.position = Startposition;
+        transform.rotation = StartRotation;
+        rb.isKinematic = true;
+        transform.localScale = new Vector3(0, 0, 0);
+        newborn = true;
+    }
+
+
+}
+    
