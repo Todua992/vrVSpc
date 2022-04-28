f0a296442a09af8e94a903652cd95224099f6b36
lukagociridze16@gmail.com <>
2022-04-28 11:22:26 +0200

    Tree mass changes, Not brabbable while growing

--- Assets/TreeRegrow.cs
+++ Assets/TreeRegrow.cs
@@ -1,9 +1,12 @@
 using UnityEngine;
+using Autohand;
 
 public class TreeRegrow : MonoBehaviour {
     [SerializeField] private float growSpeed;
 
     private Rigidbody rb;
+
+    public Grabbable grabbable;
 
     private Vector3 startPosition;
     private Quaternion startRotation;
@@ -12,23 +15,28 @@ public class TreeRegrow : MonoBehaviour {
     private bool respawnTree = false;
 
    private void Start() {
+        grabbable = GetComponent<Grabbable>();
         rb = GetComponent<Rigidbody>();
         startPosition = transform.position;
         startRotation = transform.rotation;
         startScale = transform.localScale;
 
         scaleSpeed = new Vector3(growSpeed * startScale.x, growSpeed * startScale.y, growSpeed * startScale.z);
+        rb.mass = Map(startScale.x * startScale.y * startScale.z, 0.125f, 3.375f, 10f, 40f);
     }
 
     private void Update() {
         if (transform.position.y <= -10f) {
             RespawnTree();
+            grabbable.enabled = false;
         }
 
         if (respawnTree) {
             transform.localScale += scaleSpeed * Time.deltaTime;
             if (transform.localScale.x > startScale.x) {
                 respawnTree = false;
+                grabbable.enabled = true;
+
             }
         }
     }
@@ -39,5 +47,9 @@ public class TreeRegrow : MonoBehaviour {
         transform.localScale = new Vector3(0f, 0f, 0f);
         rb.isKinematic = true;
         respawnTree = true;
+    }
+
+    private float Map(float s, float a1, float a2, float b1, float b2) {
+        return b1 + (s - a1) * (b2 - b1) / (a2 - a1);
     }
 } 
